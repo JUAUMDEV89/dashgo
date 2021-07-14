e3bded23dd211861262fb448e731402749587693
JUAUMDEV89 <jlrmd89@gmail.com>
2021-07-13 21:30:19 -0300

    First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Input } from '../components/form/input';
+
+export default function SingIn() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack  spacing="4">
+
+        <Input
+          label="E-mail"
+          name="email"
+        />
+
+        <Input
+          label="password"
+          name="password"
+        />
+
+        <Button 
+          bg="pink.500"
+          color="white"
+          size="lg"
+          mt={8}
+          w="100%"
+        >Entrar</Button>
+        
+        </Stack>
+      </Flex>
+    </Flex>
+      )
+}
