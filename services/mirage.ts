f2fa54c864ac15423319e64e1d5c04101972b994
JUAUMDEV89 <jlrmd89@gmail.com>
2022-03-07 16:09:58 -0300

    add factory and seeds

--- services/mirage.ts
+++ services/mirage.ts
@@ -1,8 +1,9 @@
-import { createServer, Model } from "miragejs"
+import { createServer, Factory, Model } from "miragejs"
+import faker from 'faker';
 
 type User = {
+    name: string;
     email: string;
-    password: string;
     created_at: string
 }
 
@@ -11,6 +12,24 @@ export function MakeServer() {
 
     models:{
         user: Model.extend<Partial<User>>({})
+    },
+
+    factories:{
+        user: Factory.extend({
+            name(i: number){
+                return `User ${i + 1}`
+            },
+            email(){
+                return faker.internet.email().toLowerCase();
+            },
+            createdAt(){
+                return faker.date.recent(10);
+            }
+        })
+    },
+
+    seeds(server){
+      server.createList('user', 200);
     },
 
     routes() {
