72cc8b657f7af613669d5191c8a9d811b893228b
JUAUMDEV89 <jlrmd89@gmail.com>
2022-03-07 15:27:02 -0300

    mirage config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
-
+import { MakeServer } from '../services/mirage';
 import { SideBarProvider } from '../contexts/SideBarContext';
+
+if(process.env.NODE_ENV === 'development'){
+  MakeServer();
+}
 
 function MyApp({ Component, pageProps }) {
   return (
